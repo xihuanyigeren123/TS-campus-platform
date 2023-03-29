d2c0ed8e36aaf9332a76ba9ffaab8093fa501360
ShangZhi <69741002+xihuanyigeren123@users.noreply.github.com>
2023-03-29 22:51:11 +0800

    feat

--- web/src/views/index.vue
+++ web/src/views/index.vue
@@ -75,17 +75,16 @@
       <el-aside style=" width: 200px;">
         <div class="nav">
           <el-menu default-active="2" class="el-menu-vertical-demo" router>
-            <el-submenu index="1">
+            <el-menu-item index="/">
               <template slot="title">
                 <i class="el-icon-position"></i>
                 <span>首页</span>
               </template>
-              <el-menu-item-group>
-                <!-- <template slot="title">分组一</template> -->
+              <!-- <el-menu-item-group>
                 <el-menu-item index="/">....</el-menu-item>
                 <el-menu-item index="1-2">...</el-menu-item>
-              </el-menu-item-group>
-            </el-submenu>
+              </el-menu-item-group> -->
+            </el-menu-item>
 
             <el-submenu index="2">
               <template slot="title">
